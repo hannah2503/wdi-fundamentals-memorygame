7fca648a661420696a723ce49afb577653d39854
Hannah Cross <hannahbmcross@gmail.com>
2017-09-07 11:58:48 +0100

    added to game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,5 +1,26 @@
 console.log("Up and running!");
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
 var cardsInPlay = [];
 var checkForMatch = function() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
@@ -10,8 +31,10 @@ console.log("Sorry, try again.");
 }
 var flipCard = function(cardId){
 	
-	console.log("User flipped " + cards[cardId]);
-		cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+		cardsInPlay.push(cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 	
 		if (cardsInPlay.length === 2) {
 			checkForMatch();
