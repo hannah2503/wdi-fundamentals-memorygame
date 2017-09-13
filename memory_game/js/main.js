ce45203ae071ddaa008e21e906769c2e74a55847
Hannah Cross <hannahbmcross@gmail.com>
2017-09-13 15:08:24 +0100

    finalversion

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -29,12 +29,13 @@ console.log("You found a match!");
 console.log("Sorry, try again.");
 	}
 }
-var flipCard = function(cardId){
-	
+var flipCard = function(){
+	var cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
 		cardsInPlay.push(cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
+	this.setAttribute('src', (cards[cardId].cardImage));
 	
 		if (cardsInPlay.length === 2) {
 			checkForMatch();
@@ -43,5 +44,16 @@ var flipCard = function(cardId){
 		} else {alert("Sorry, try again")}
 	}
 }
-flipCard(0);
-flipCard(2);
+var createBoard = function(){
+	for (var i = 0; i < cards.length; i++){
+	var cardElement = document.createElement('img');
+	cardElement.setAttribute('src' , "images/back.png");
+	cardElement.setAttribute('data-id', i);
+	cardElement.addEventListener('click' , flipCard);
+	document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+
+createBoard();
+
+
