b4dcd51b3c0408a77e0bf21b2b987ff6230b5b06
Hannah Cross <hannahbmcross@gmail.com>
2017-09-21 14:26:31 +0100

    updated code to include game refresh

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,42 +1,47 @@
+
 console.log("Up and running!");
+
 var cards = [
-{
-	rank: "queen",
-	suit: "hearts",
-	cardImage: "images/queen-of-hearts.png"
-},
-{
-	rank: "queen",
-	suit: "diamonds",
-	cardImage: "images/queen-of-diamonds.png"
-},
-{
-	rank: "king",
-	suit: "hearts",
-	cardImage: "images/king-of-hearts.png"
-},
-{
-	rank: "king",
-	suit: "diamonds",
-	cardImage: "images/king-of-diamonds.png"
-}
-];
+	{
+		rank: "queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png"
+	},
+	{
+		rank: "queen",
+		suit: "diamonds",
+		cardImage: "images/queen-of-diamonds.png"
+	},
+	{
+		rank: "king",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png"
+	},
+	{
+		rank: "king",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds.png"
+	}
+	];
+
 var cardsInPlay = [];
+
 var checkForMatch = function() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 console.log("You found a match!");
 	} else {
 console.log("Sorry, try again.");
+playAgain();
 	}
 }
+
 var flipCard = function(){
 	var cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
-		cardsInPlay.push(cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
 	this.setAttribute('src', (cards[cardId].cardImage));
-	
 		if (cardsInPlay.length === 2) {
 			checkForMatch();
 		if (cardsInPlay[0] === cardsInPlay[1]){
@@ -44,6 +49,13 @@ var flipCard = function(){
 		} else {alert("Sorry, try again")}
 	}
 }
+
+var playAgain = function(){
+	location.reload();
+}
+
+document.getElementById('reset-button').addEventListener('click' , playAgain);
+
 var createBoard = function(){
 	for (var i = 0; i < cards.length; i++){
 	var cardElement = document.createElement('img');
@@ -55,5 +67,9 @@ var createBoard = function(){
 }
 
 createBoard();
+
+
+
+
 
 
